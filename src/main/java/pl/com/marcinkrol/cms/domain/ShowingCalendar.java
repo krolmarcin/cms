21367df5838a922bb28f8b1ade7aa13121a86ded
Marcin <szeryfmk@gmail.com>
2017-09-26 10:57:43 +0200

    add showings for dates

--- src/main/java/pl/com/marcinkrol/cms/domain/ShowingCalendar.java
+++ src/main/java/pl/com/marcinkrol/cms/domain/ShowingCalendar.java
@@ -1,5 +1,7 @@
 package pl.com.marcinkrol.cms.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.time.DayOfWeek;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -7,12 +9,46 @@ import java.util.List;
 
 public class ShowingCalendar {
 
+    @JsonFormat(pattern = "yyyy/MM/dd HH:mm")
     private LocalDateTime fromDate;
+
+    @JsonFormat(pattern = "yyyy/MM/dd HH:mm")
     private LocalDateTime untilDate;
 
     private List<DayOfWeek> weekDays;
+
     private List<LocalTime> hours;
 
-    
+    public LocalDateTime getFromDate() {
+        return fromDate;
+    }
+
+    public void setFromDate(LocalDateTime fromDate) {
+        this.fromDate = fromDate;
+    }
+
+    public LocalDateTime getUntilDate() {
+        return untilDate;
+    }
+
+    public void setUntilDate(LocalDateTime untilDate) {
+        this.untilDate = untilDate;
+    }
+
+    public List<DayOfWeek> getWeekDays() {
+        return weekDays;
+    }
+
+    public void setWeekDays(List<DayOfWeek> weekDays) {
+        this.weekDays = weekDays;
+    }
+
+    public List<LocalTime> getHours() {
+        return hours;
+    }
+
+    public void setHours(List<LocalTime> hours) {
+        this.hours = hours;
+    }
 
 }
